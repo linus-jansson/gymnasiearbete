aa730f813016ec3017125ebb9f9441c56f0b34c3
Linus Jansson <linus.jansson2000@gmail.com>
2021-09-18 09:49:27 +0200

    Added new way

---
+++ python/main_new.py
@@ -0,0 +1,89 @@
+import pygame, sys, time
+
+
+class Ball:
+    def __init__(self, screen, color, x, y, radius):
+        self.screen = screen
+        self.color = color
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.show()
+
+        self.xSpeed = 1
+        self.ySpeed = 0
+
+    def show(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+
+    def update(self):
+        if self.x > WIDTH:
+            self.x = WIDTH-1
+            self.xSpeed = -1
+        elif self.x < WIDTH:
+            self.x = 1
+            self.xSpeed = 1
+
+        self.x += self.xSpeed
+        self.y += self.ySpeed
+    
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+class Paddle:
+    def __init__(self, screen, color, x, y, width, height):
+        self.screen = screen
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.show()
+
+    
+    def show(self):
+        pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
+
+    def update(self, newX, newY):
+        # if (self.y + newY) > screen.width: 
+        pygame.draw.rect(self.screen, self.color, (self.x, self.y + newY, self.width, self.height))
+
+        self.y += newY
+
+        
+
+pygame.init()
+
+WIDTH = 900
+HEIGHT = 500
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PONG")
+
+
+def mid_line():
+    pygame.draw.line(screen, WHITE, (WIDTH//2, 0), (WIDTH//2, HEIGHT), 5)
+    
+
+ball = Ball(screen, WHITE, WIDTH//2, HEIGHT//2 , 15)
+paddle1 = Paddle(screen, WHITE, 15, HEIGHT//2 - 60, 20, 120)
+paddle2 = Paddle( screen, WHITE, WIDTH - 20 - 15, HEIGHT//2 - 60, 20, 120 )
+
+def update():
+    screen.fill(BLACK)
+    ball.update()
+    paddle1.update(0, 0)
+    paddle2.update(0, 0)
+    mid_line()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    time.sleep(0.5)
+    update()
+
+    pygame.display.update()
