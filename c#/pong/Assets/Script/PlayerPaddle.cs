5d484b95d304b1f7bc126297f62e656b1846b5b7
villegust <ville.gust@gmail.com>
2021-12-03 15:47:53 +0100

    Det Linus gjorde

--- c#/pong/Assets/Script/PlayerPaddle.cs
+++ c#/pong/Assets/Script/PlayerPaddle.cs
@@ -3,39 +3,83 @@ using UnityEngine;
 public class PlayerPaddle : Paddle
 {
     public Vector2 direction { get; private set; }
+    public string pos;
 
     private void Update()
     {
+        switch (this.pos)
+        {
+            case "up":
+                this.movePaddleTop();
+                break;
+            case "middle":
+                this.movePaddleMiddle();
+                break;
+            case "down":
+                this.movePaddleBottom();
+                break;
+        }
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            //this.direction = Vector2.up;
-            this.movePaddleTop();
+            //this.direction = Vector2.up
+            if (this.pos == "down")
+            {
+                this.pos = "middle";
+            }
+            else if (this.pos == "middle")
+            {
+                this.pos = "up";
+            }
+            else
+            {
+                this.pos = "up";
+            }
         }
         else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            //this.direction = Vector2.down;
-            this.movePaddleBottom();
+            //this.direction = Vector2.down
+            if (this.pos == "up")
+            {
+                this.pos = "middle";
+            }
+            else if (this.pos == "middle")
+            {
+                this.pos = "down";
+            }
+            else
+            {
+                this.pos = "down";
+            }
         }
-        else
-        {
-            //this.direction = Vector2.zero;
-            this.movePaddleMiddle();
-        }
+        //else
+        //{
+        //    //this.direction = Vector2.zero;
+        //    this.pos = "middle";
+        //}
+
     }
 
     public void movePaddleTop()
     {
+        Debug.Log("up");
         this.rigidbody.position = new Vector2(this.rigidbody.position.x, 3.4f);
+        this.pos = "up";
     }
 
     public void movePaddleMiddle()
     {
         this.rigidbody.position = new Vector2(this.rigidbody.position.x, 0f);
+        Debug.Log("down");
+        this.pos = "middle";
+
     }
 
     public void movePaddleBottom()
     {
         this.rigidbody.position = new Vector2(this.rigidbody.position.x, -3.4f);
+        Debug.Log("middle");
+        this.pos = "down";
     }
 
     private void FixedUpdate()
